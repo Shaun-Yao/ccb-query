72e5efb73ca8e4cbdbd38b74b3b457b6ba411bdc
yao <349638972@qq.com>
2020-11-27 14:32:21 +0800

    1.店面结余公式加上现金调整 2.存款填写加上日期范围限制

--- src/main/java/com/honji/order/mapper/CashBalanceMapper.java
+++ src/main/java/com/honji/order/mapper/CashBalanceMapper.java
@@ -26,7 +26,8 @@ public interface CashBalanceMapper extends BaseMapper<CashBalance> {
             ") balance\n" +
             "LEFT JOIN\n" +
             "(\n" +
-            "SELECT sum(ISNULL(cash, 0) - ISNULL(deposit, 0) + ISNULL(extra_cash, 0)) as amount FROM daily_deposit deposit \n" +
+            "SELECT sum(ISNULL(cash, 0) - ISNULL(deposit, 0) + ISNULL(extra_cash, 0)+ ISNULL(cash_adjustment, 0)) as amount " +
+            "FROM daily_deposit deposit \n" +
             "LEFT JOIN cash_balance balance \n" +
             "ON deposit.khdm = balance.khdm \n" +
             "WHERE deposit.khdm = #{shopCode} AND deposit.date >= balance.date\n" +
